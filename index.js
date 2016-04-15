46050e678cc3aaa341a1614052588e1152b057c0
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-16 01:51:13 +0200

    use express v4 and serve-index

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-// app.js
+// Bootstrap needs
 var http = require('http'),
     fs = require('fs'),
     express = require('express'),
     bodyParser = require('body-parser'),
     mysql = require('mysql'),
-    ejs = require('ejs');
+    ejs = require('ejs'),
+    sIndex = require('serve-index'),
+    port = 3550;
 
+// Define app as express app
 var app = express();
 
-app.use('/', express.static(__dirname + '/reports'));
-app.use('/images', express.static(__dirname + '/public/images'));
-app.use('/images',express.directory(__dirname + '/public/images'));
+app.use('/', sIndex(__dirname + '/reports'));
+
+// Reports root
+// app.get('/', function rootMiddleware(req, res, next) {
+//   // res.send('Hello World!');
+//   express.bodyParser();
+// });
+
+// Define port to listen to
+app.listen(3550, function() {
+  console.log("I'm listening on "+port+"...");
+});
