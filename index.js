14cb8ef697dc4a5188909950ca84091065bf9e95
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-17 19:39:41 +0200

    remove non used dependencies

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Bootstrap needs
-var http = require('http'),
-    fs = require('fs'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    mysql = require('mysql'),
-    ejs = require('ejs'),
+var express = require('express'),
     sIndex = require('serve-index'),
     port = 3550,
     rootPath = __dirname + '/reports';
@@ -14,7 +9,7 @@ var app = express();
 
 // Get static files from reports
 // Allow navigate throw reports directories
-var defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath)];
+var defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath, {'icons': true})];
 app.use('/', defaultRouteFunctions);
 
 // Define port to listen to
