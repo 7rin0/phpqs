40059b7a4a81eac182c39e12973b5be192ac9a95
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-20 04:05:07 +0200

    add semicolons

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ var express,
   rootPath,
   app,
   server,
-  defaultRouteFunctions
+  defaultRouteFunctions;
 
 // set var values
-express = require('express')
-sIndex = require('serve-index')
-io = require('socket.io')
-port = 3550
-rootPath = __dirname + '/reports'
-app = express()
-io = io(server)
+express = require('express');
+sIndex = require('serve-index');
+io = require('socket.io');
+port = 3550;
+rootPath = __dirname + '/reports';
+app = express();
+io = io(server);
 
 // Define port to listen to
 server = app.listen(port, function () {
-  console.log('I\'m listening on http://localhost:' + port + '...')
-})
+  console.log('I\'m listening on http://localhost:' + port + '...');
+});
 
 // Get static files from reports
 // Allow navigate throw reports directories
-defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath, {'icons': true})]
-app.use('/', defaultRouteFunctions)
+defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath, {'icons': true})];
+app.use('/', defaultRouteFunctions);
