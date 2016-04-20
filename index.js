c802a3c5b5a87c4672070dc760b086e36876101c
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-20 04:00:37 +0200

    apply js lint

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// Bootstrap needs
-var express = require('express'),
-    sIndex = require('serve-index'),
-    io = require('socket.io'),
-    port = 3550,
-    rootPath = __dirname + '/reports';
+// var declaration
+var express,
+  sIndex,
+  io,
+  port,
+  rootPath,
+  app,
+  server,
+  defaultRouteFunctions
 
-// Define app as express app
-var app = express();
+// set var values
+express = require('express')
+sIndex = require('serve-index')
+io = require('socket.io')
+port = 3550
+rootPath = __dirname + '/reports'
+app = express()
+io = io(server)
 
 // Define port to listen to
-var server = app.listen(port, function() {
-  console.log("I'm listening on http://localhost:"+port+"...");
-});
-
-// update io
-io = io(server);
+server = app.listen(port, function () {
+  console.log('I\'m listening on http://localhost:' + port + '...')
+})
 
 // Get static files from reports
 // Allow navigate throw reports directories
-var defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath, {'icons': true})];
-app.use('/', defaultRouteFunctions);
+defaultRouteFunctions = [express.static(rootPath), sIndex(rootPath, {'icons': true})]
+app.use('/', defaultRouteFunctions)
